87e9410292adb7b7744f6141fbb831a4abb5eaca
KayRams <ramolotoingrid@gmail.com>
2025-08-29 17:40:08 +0200

    Added the synonyms component

---
+++ my-app/src/Synonyms.js
@@ -0,0 +1,22 @@
+import React from "react";
+
+export default function Synonyms(props) {
+  if (props.synonyms.length > 0) {
+    return (
+      <div className="Synonyms">
+        <strong>Synonyms:</strong>
+        {props.synonyms.map(function (synonym, index) {
+          return (
+            <span key={index}>
+              {" "}
+              {synonym}
+              {index < props.synonyms.length - 1 ? "," : ""}
+            </span>
+          );
+        })}
+      </div>
+    );
+  } else {
+    return null;
+  }
+}
